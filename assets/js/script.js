b158cea385e2de2454d41826feb0eefac4bb3ccd
Victor Pestana <victorpestanavgp@gmail.com>
2025-02-11 12:18:08 -0300

    :sparkles: Conclusao Portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+const sobre = document.querySelector("#sobre");
+const formulario = document.querySelector("#formulario");
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+async function getApiGithub() {
+	try {
+		const response = await fetch("https://api.github.com/users/victorpestana");
+		const perfil = await response.json();
+
+		let conteudo = `
+			<!-- Seção sobre -->
+			<section id="about" class="flex sobre_container">
+				<!-- Imagem da Seção Sobre -->
+				<img src="${perfil.avatar_url}" alt="Foto do Perfil do Github">
+				<!-- Texto da Seção Sobre -->
+				<article id="about_texto">
+					<h2>Sobre mim</h2>
+					<p>Jovem desenvolvedor FullStack Javascript de 19 anos, buscando sua primeira oportunidade na área.</p>
+					<!-- Detalhes do Github -->
+					<div id="about_github" class="flex sobre_github">
+						<a href="https://github.com/VictorPestana" target="_blank" class="botao">
+							Github
+						</a>
+						<p>${perfil.followers} Seguidores</p>
+						<p>${perfil.public_repos} Repositórios</p>
+					</div>
+				</article>
+			</section>
+		`;
+
+		sobre.innerHTML += conteudo;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+ 
+    const campoNome=document.querySelector("#nome");
+    const txtNome=document.querySelector("#txtNome")
+ 
+    if(campoNome.value.length<3){
+        txtNome.innerHTML="O nome deve ter no mínimo 3 caracteres."
+        campoNome.focus();
+        return;
+    }else{
+        txtNome.innerHTML="";
+    }
+ 
+    const campoEmail=document.querySelector("#email");
+    const txtEmail=document.querySelector("#txtEmail")
+ 
+    if(!campoEmail.value.match(emailRegex)){
+        txtEmail.innerHTML="Digite um e-mail válido."
+        campoEmail.focus();
+        return;
+    }else{
+        txtEmail.innerHTML="";
+    }
+ 
+    const campoAssunto=document.querySelector("#assunto");
+    const txtAssunto=document.querySelector("#txtAssunto")
+ 
+    if(campoAssunto.value.length<5){
+        txtAssunto.innerHTML="O assunto deve ter no mínimo 5 caracteres."
+        campoAssunto.focus();
+        return;
+    }else{
+        txtAssunto.innerHTML="";
+    }
+ 
+    formulario.submit();
+})
+ 
+getApiGitHub();
